f040a2c20b944385f9c29795b82a678d1e1d01ca
Mohamat-tech <ndaoba26@gmail.com>
2026-05-30 07:08:57 +0100

    Create next.config.js

---
+++ frontend/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
+  images: {
+    domains: ["proxypharma-backend.onrender.com"],
+  },
+}
+
+module.exports = nextConfig
